3cbec4da3b14d84a7c1b1d5d69ce7a23ab446a93
Angus Hardie <Angus.Hardie@malcolmhardie.com>
2013-07-13 00:32:44 +0100

    main.m

--- SVGExperiment/main.m
+++ SVGExperiment/main.m
@@ -3,7 +3,7 @@
 //  SVGExperiment
 //
 //  Created by Angus Hardie on 12/07/2013.
-//  Copyright (c) 2013 MalcolmHardie Solutions. All rights reserved.
+//  Copyright (c) 2013 Angus Hardie. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
